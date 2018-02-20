a85b01c84f99df83f0d4b38a0cdd3b7a9ce08c15
Teis <teisgossen@gmail.com>
2018-02-20 14:24:39 +0100

    chapter 4 else statement
    Added and if and else statement that checks if the input number is negative or not.

--- Jumping_Into_C++/Chapter4/Chapter4/main.cpp
+++ Jumping_Into_C++/Chapter4/Chapter4/main.cpp
@@ -12,7 +12,19 @@ int main() {
 		cout << "You entered a value less than 10" << '\n';
 	}
 
+	cout << "\n";
 
+	//If and else stament that check if the input number is negative or not
+	int num;
+
+	cout << "Enter a number: ";
+	cin  >> num;
+	if (num < 0) {
+		cout << "You entered a negative number\n";
+	}
+	else {
+		cout << "You entered a non-negative number\n";
+	}
 
 	cin.get();
 }

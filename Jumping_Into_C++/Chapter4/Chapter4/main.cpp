0f623d6287f28fd14f142026931d7841124f0a1b
Teis <teisgossen@gmail.com>
2018-02-20 14:20:54 +0100

    chapter 4 start
    Started chapter 4. We start off with a simple if statement.

---
+++ Jumping_Into_C++/Chapter4/Chapter4/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//If statement that check if the input value is less than 10
+	int x;
+	
+	cout << "Enter a number: ";
+	cin  >> x;
+	if (x < 10) {
+		cout << "You entered a value less than 10" << '\n';
+	}
+
+
+
+	cin.get();
+}

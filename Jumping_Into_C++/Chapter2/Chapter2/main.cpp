047352f901b04c7dffbf12ebfa955b6d641dcca9
Teis <teisgossen@gmail.com>
2018-02-20 11:58:14 +0100

    Finished chapter 2
    Finished making chapter 2 f Jumping Inot C++

---
+++ Jumping_Into_C++/Chapter2/Chapter2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hey, I'm alive! Oh, and Hello World!\n";
+
+	//Practice problems 1:
+	cout << "My name is Teis.\n";
+
+	//Practice problem 2:
+	cout << "My friends are:\n";
+	cout << "Robin\n";
+	cout << "Jerry\n";
+	cout << "Erwin";
+
+	cin.get();
+}
